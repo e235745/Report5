daaf9e141774f0a699e44bbcfd912038592a28e8
松永敏実 <e235745@matsunagasatoshijitsunoMacBook-Air.local>
2023-12-08 16:27:21 +0900

    yazasi

--- src/jp/ac/uryukyu/ie/e235745/Main.java
+++ src/jp/ac/uryukyu/ie/e235745/Main.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e235745;
 
 public class Main {
     public static void main(String[] args){
-      String str = null;
-      System.out.println(str.length());  
+        try{
+        String str = null;
+        System.out.println(str.length());  
+        }catch(NullPointerException x){
+            System.out.println("NullPointerExceptionのエラーが発生しました");
+            System.out.println(x.getMessage());
+        }
     }
 }
